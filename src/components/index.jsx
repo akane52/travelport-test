38bc5e3b292b709b30a287c33e422836dbb0e902
akane52 <lulomo@bemen3.cat>
2023-01-03 15:00:27 +0100

    Prt 1

---
+++ src/components/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {  Container, CardLastName, CardFirstName, CardEmail, CardImage} from "./styles";
+import { data } from '../dataSamples/personalData';
+
+const PersonalCards = () =>{
+    return (
+        <div>
+        {data.map(item => {
+            return(
+                <Container>
+                    <CardImage src={item.picture.medium}/>
+                        <CardFirstName>{item.name.first}</CardFirstName>
+                        <CardLastName>{item.name.last}</CardLastName>
+                    <CardEmail>{item.email}</CardEmail>
+                </Container>
+            )
+        })}
+      </div>
+    )
+}
+export default PersonalCards
