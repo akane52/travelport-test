374f4e48cc12424d754d8cfbb4a55aac73c04e72
akane52 <lulomo@bemen3.cat>
2023-01-04 13:03:39 +0100

    Part3

--- src/components/PersonalCards/styles.js
+++ src/components/PersonalCards/styles.js
@@ -3,19 +3,29 @@ import styled from "@emotion/styled";
 export const Container = styled.div`
     display: flex;
 `
+export const InformationContainer = styled.div`
+    height: fit-content;
+    margin-left: 1%;
+`
+export const NameSurnameContainer = styled.div`
+    display: flex;
+    justify-content: center;
+`
+export const EmailContainer = styled.div`
+    display: flex;
+`
 export const CardFirstName = styled.p`
     font-weight: bold;
-    margin-left: 3%;
 `
 export const CardLastName = styled.p`
     font-weight: bold;
-    margin-left: 0.5%;
+    margin-left: 3%;
 `
 export const CardEmail = styled.p`
     margin-top: 3%;
-    margin-left: -10%;
-    display: flex;
 `
 export const CardImage = styled.img`
     margin-top: 5px;
+    width: 5rem;
+    height: 5rem;
 `
