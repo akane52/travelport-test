df29274fae85b305e6400170fa63294b1c840146
akane52 <lulomo@bemen3.cat>
2023-01-04 12:40:30 +0100

    Part3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import Button from './components/Button';
 import PersonalCards from './components/PersonalCards';
-import { data } from './dataSamples/personalData';
-import React, { useState } from 'react';
+// import { data } from './dataSamples/personalData';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 
 let count = 0;
+const URL = "https://randomuser.me/api/?results=5";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get(URL).then((response) => {
+      setData(response.data.results);
+    });
+  }, []);
+
+  console.log(data);
+
   const [showComponent, setShowComponent] = useState([]);
   const showPersonalCards = () =>{
     if (count === data.length) {
